1ce6486c1b6e3db6253f3fec3bf85fa0653b46ea
deazth <deazth@gmail.com>
2017-08-09 21:47:46 +0800

    search order full

--- order/o_search.php
+++ order/o_search.php
@@ -13,11 +13,43 @@
         $obj = json_decode($json);
 
         $arrecords = $obj->getInfoOrder->records;
+        $acc_name = "";
+        $order_type = "";
+        $order_status = "";
 
-        print_r($arrecords);
+        $svcsarray = array();
+        $reccount = 0;
 
+        foreach($arrecords as $srecord){
+            $reccount++;
+
+            $acc_name = $srecord[1];
+            $order_status = $srecord[2];
+            $svc_id = $srecord[3];
+            $prod_code = $srecord[4];
+            $prod_desc = $srecord[5];
+            $prod_name = $srecord[6];
+
+            $svc = array(
+                "svc_id" => $srecord[3],
+                "product_name" => $srecord[6]
+            )
+
+            array_push($svcsarray, $svc);
+        }
+
+        if($reccount > 0){
+            array_push($returnarr, array("acc_name"=>$acc_name));
+            array_push($returnarr, array("order_type"=>$order_type));
+            array_push($returnarr, array("order_status"=>$order_status));
+            array_push($returnarr, array("services"=>$svcsarray));
+        } else {
+            array_push($returnarr, array("error"=>"record not found"));
+        }
+    } else {
+        array_push($returnarr, array("error"=>"bad input"));
     }
 
-    // echo json_encode($returnarr);
+    echo json_encode($returnarr);
 
 ?>
