2c810d31656843289b9218ebe524ab96311ea59f
deazth <deazth@gmail.com>
2017-08-09 18:14:24 +0800

    actual api call

--- ex_callapi.php
+++ ex_callapi.php
@@ -1,9 +1,17 @@
 <?php
 
-    print_r($_GET);
-    if($_GET["a"] === "") echo "a is an empty string\n";
-    if($_GET["a"] === false) echo "a is false\n";
-    if($_GET["a"] === null) echo "a is null\n";
-    if(isset($_GET["a"])) echo "a is set\n";
-    if(!empty($_GET["a"])) echo "a is not empty";
+    // print_r($_GET);
+    // if($_GET["a"] === "") echo "a is an empty string\n";
+    // if($_GET["a"] === false) echo "a is false\n";
+    // if($_GET["a"] === null) echo "a is null\n";
+    // if(isset($_GET["a"])) echo "a is set\n";
+    // if(!empty($_GET["a"])) echo "a is not empty";
+
+
+    $theurl = 'http://itnthackathon.bweas.tm.com.my/api/getBillAcc?filter=input,eq,nazlinas@unifi';
+
+    $json = file_get_contents($theurl);
+    $obj = json_decode($json);
+    echo $obj->access_token;
+
 ?>
